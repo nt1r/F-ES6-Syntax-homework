1e4313ce8f0a135180c366fd59b054f5692397bc
Leqi <michaelleqihust@gmail.com>
2020-09-11 18:38:14 +0800

    [leqi] feat: finish homework.

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,25 @@
-const parseData
+const parseData = (input) => {
+  const {data, column} = input;
+
+  const firstColumnKeyName = Object.keys(Object.values(column)[0])[0];
+  const propertyNameList = [];
+  for (let dataName of column) {
+    propertyNameList.push(dataName[firstColumnKeyName]);
+  }
+
+  const parsedResultList = [];
+  for (let valueList of data) {
+    let parsedObject = {}
+    let propertyIndex = 0;
+    for (let value of valueList) {
+      parsedObject = {
+        ...parsedObject,
+        [propertyNameList[propertyIndex++]]: value,
+      }
+    }
+    parsedResultList.push(parsedObject);
+  }
+
+  return parsedResultList;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,14 @@
-const inject
+const inject = (items, sections) => {
+  sortByIndexDescending(sections)
+
+  for (let section of sections) {
+    items.splice(section.index, 0, section.content);
+  }
+
+  return items;
+}
+
+const sortByIndexDescending = (sections) => {
+  sections.sort((first, second) => second.index - first.index);
+}
 export { inject };
