409fac851766e2076da1b2a6362a3868c8292cf1
kevindongzg <dongkevin94@gmail.com>
2020-09-22 15:25:15 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,7 +1,11 @@
+// TODO 方法体过长，思考一下可否优化
 const parseData = (input) => {
+  // TODO 解构可以优化到函数接受参数中
   const {data, column} = input;
 
+  // TODO 这里‘name’可以认为是固定的key值，这属于前后端约定的一部分，所以不需要 firstColumnKeyName
   const firstColumnKeyName = Object.keys(Object.values(column)[0])[0];
+  // TODO 使用map做数组与数组的转化，不用声明新数组
   const propertyNameList = [];
   for (let dataName of column) {
     propertyNameList.push(dataName[firstColumnKeyName]);
@@ -10,6 +14,7 @@ const parseData = (input) => {
   const parsedResultList = [];
   for (let valueList of data) {
     let parsedObject = {}
+    // 不用重新声明 index，使用valueList.entries或forEach
     let propertyIndex = 0;
     for (let value of valueList) {
       parsedObject = {
--- src/practice2.js
+++ src/practice2.js
@@ -1,3 +1,4 @@
+// TODO 做的不错
 const inject = (items, sections) => {
   sortByIndexDescending(sections)
 
